3b259898ed98c626f57aa12fd44fe9a5e3741901
Richard Dymond <rjdymond@gmail.com>
2017-01-24 17:52:32 -0400

    Use Python 3

--- utils/mkasm.py
+++ utils/mkasm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from skrunner import run_skool2asm
 
 run_skool2asm()
--- utils/mkhtml.py
+++ utils/mkhtml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from skrunner import run_skool2html
 
 run_skool2html()
