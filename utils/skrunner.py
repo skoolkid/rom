8c5e0b8b5973b7cceb2378a2f83c4d45db142cc1
Richard Dymond <rjdymond@gmail.com>
2017-04-21 18:26:42 -0300

    Remove the '-c' options from run_skool2html() in utils/skrunner.py

--- utils/skrunner.py
+++ utils/skrunner.py
@@ -24,9 +24,5 @@ def run_skool2asm():
     skool2asm.main(['-H'] + sys.argv[1:] + [ROM_SKOOL])
 
 def run_skool2html():
-    options = [
-        '-c', 'Game/AddressAnchor={address:04X}',
-        '-c', 'Paths/CodeFiles={address:04X}.html',
-        '-Had', '{}/build/html'.format(ROM_HOME)
-    ]
-    skool2html.main(options + sys.argv[1:] + [ROM_SKOOL])
+    options = '-Had {}/build/html'.format(ROM_HOME)
+    skool2html.main(options.split() + sys.argv[1:] + [ROM_SKOOL])
